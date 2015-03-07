082a929bed2bb54abaecf74b0856b74e694e002c
Mariko <marikosakaii@gmail.com>
2015-03-08 00:04:36 +0800

    Added link detection

---
+++ db/migrate/20150307155127_add_content_htmlto_tasks.rb
@@ -0,0 +1,4 @@
+class AddContentHtmltoTasks < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150307041616) do
+ActiveRecord::Schema.define(version: 20150307155127) do
 
   create_table "tasks", force: true do |t|
     t.text     "content"
