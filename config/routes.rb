671027c2a02ccc0a14c1a352f592bf7c68bab3a4
Amanda Mariko Sakai <marikosakaii@gmail.com>
2015-06-25 19:28:33 +0800

    Revamp About page, sign in page, task index page, sign up page; WIP show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   resources :tasks do
     member do
       put :change
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
   get 'about' => 'pages#about'
 
   get 'test' => 'pages#test'
+
+  
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
