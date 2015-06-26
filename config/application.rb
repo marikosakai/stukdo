710364a88b68d56b4e80060ef1e5667710132167
Amanda Mariko Sakai <marikosakaii@gmail.com>
2015-06-26 17:43:47 +0800

    Add assets precompile

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module Stukdo
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf|otf)\z/
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
